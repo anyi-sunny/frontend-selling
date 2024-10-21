08c9e3d06a42863bd7d1db9d9fc98f6fe5eb513d
anyi-sunny <asunnysky65@gmail.com>
2024-10-21 08:23:32 -0400

    navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Home from './components/Home'
+import MenuBar from './components/MenuBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <MenuBar/>
+      <Home/>
       <div>
-        <Home/>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
