f3ffb069e79c08543c109568c71e5e9eda8a2992
Anthony <aholly@bowdoin.edu>
2024-10-24 11:16:21 -0400

    HTML => JSX for bootsrap

--- client/src/components/MenuBar.jsx
+++ client/src/components/MenuBar.jsx
@@ -1,43 +1,71 @@
-import React from "react";
+// import React from "react";
+// import Container from 'react-bootstrap/Container';
+// import Nav from 'react-bootstrap/Nav';
+// import Navbar from 'react-bootstrap/Navbar';
+// import NavDropdown from 'react-bootstrap/NavDropdown';
+// import "./MenuBar.css";
+
+
+// export default function MenuBar () {
+//     return (
+//         <div>
+//         <nav className="navbar navbar-expand-lg navbar-warning bg-warning" style={{ padding: 20 }}>
+//   <a class="navbar-brand navbar-warning bg-warning" href="#">Bowdoin Reselling</a>
+//   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+//     <span class="navbar-toggler-icon"></span>
+//   </button>
+//   <div class="collapse navbar-collapse navbar-warning bg-warning" id="navbarNavDropdown">
+//     <ul class="navbar-nav">
+//       <li class="nav-item active navbar-warning bg-warning">
+//         <a class="nav-link navbar-warning bg-warning" href="#">Home <span class="sr-only">(current)</span></a>
+//       </li>
+//       <li class="nav-item navbar-warning bg-warning">
+//         <a class="nav-link" href="#">Features</a>
+//       </li>
+//       <li class="nav-item navbar-warning bg-warning">
+//         <a class="nav-link" href="#">Pricing</a>
+//       </li>
+
+//       <li class="nav-item dropdown navbar-warning bg-warning" style={{ float: "right"}}> 
+//         <a class="nav-link" href="#">Account</a>
+//       </li>
+  
+//     </ul>
+//   </div>
+// </nav>
+//         </div>
+//     )
+// }
+
+import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import "./MenuBar.css";
+import './MenuBar.css';
 
-
-export default function MenuBar () {
-    return (
-        <div>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand navbar-dark bg-dark" href="#">Bowdoin Reselling</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse navbar-dark bg-dark" id="navbarNavDropdown">
-    <ul class="navbar-nav">
-      <li class="nav-item active navbar-dark bg-dark">
-        <a class="nav-link navbar-dark bg-dark" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item navbar-dark bg-dark">
-        <a class="nav-link" href="#">Features</a>
-      </li>
-      <li class="nav-item navbar-dark bg-dark">
-        <a class="nav-link" href="#">Pricing</a>
-      </li>
-      <li class="nav-item dropdown navbar-dark bg-dark">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Account
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="#">Settings</a>
-          <a class="dropdown-item" href="#">Items</a>
-          <a class="dropdown-item" href="#">Likes</a>
-        </div>
-      </li>
-    </ul>
-  </div>
-</nav>
-        </div>
-    )
+export default function MenuBar() {
+  return (
+    <Navbar bg="warning" expand="lg" style={{ padding: 20 }}>
+      <Container>
+        <Navbar.Brand href="#">Bowdoin Reselling</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarNavDropdown" />
+        <Navbar.Collapse id="navbarNavDropdown">
+          <Nav className="me-auto">
+            <Nav.Link href="#">Home</Nav.Link>
+            <Nav.Link href="#">Features</Nav.Link>
+            <Nav.Link href="#">Pricing</Nav.Link>
+          </Nav>
+          <Nav>
+            <NavDropdown title="Account" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#">Profile</NavDropdown.Item>
+              <NavDropdown.Item href="#">Settings</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#">Logout</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
